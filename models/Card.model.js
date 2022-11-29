34618a9d091cb4f8713e9d5a81aecbe4412e1538
Deny Nakano <deny.nakano@gmail.com>
2022-11-29 19:49:35 -0300

    models done

--- models/Card.model.js
+++ models/Card.model.js
@@ -1,6 +1,51 @@
 import {Schema, model} from 'mongoose'
 
 const cardSchema = new Schema({
-    
+    name:{
+        type: String,
+        required: [true, 'card name is required']
+    },
+    manaCost:{
+        type: String,
+    },
+    cmc:{
+        type: Number,
+    },
+    color:{
+        type: String,
+    },
+    types:{
+        type: String,
+    },
+    subTypes:{
+        type: [ String ],
+    },
+    rarity:{
+        type: String,
+    },
+    set:{
+        type: String,
+    },
+    setName:{
+        type: String,
+    },
+    text:{
+        type: String,
+    },
+    imageUrl:{
+        type: String,
+    },
+    legalities:{
+        type: [ String ],
+    },
+    id:{
+        type: String,
+    },
+    idSpellseeker:{
+        type: String,
+    },
+    commander:{
+        type: Boolean,
+    }
 
-}, {timestemp: true})
+}, {timestamps: true})
