2cf91e548938cac658eba4671c30a4a38a55cdc6
EuripedesNaves <euripedes.naves@outlook.com>
2022-12-06 20:13:11 -0300

    feat: search card model

--- app.js
+++ app.js
@@ -5,42 +5,34 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 const authRoute = require('./routes/auth.routes.js');
+const card = require('./routes/card.routes.js');
 
 const app = express();
 
 //Conectado BD
 require('./db/index.js');
 
+
 // Conexões de outras origens
 app.use(cors())
 
+
 //Em desenvolvimento show app logs
 app.use(logger('dev'))
-
 
 
 //Body das Requisições
 app.use(express.json())
 
 
-//Crud SpellSeeker
-app.get('/', (request, response) => {
-    response.send('Server Online')
-})
-
-// CREATE
-app.post('/card', (req, res) => {
-    res.send('Criando Cards')
-})
-
 //Rotas Públicas
 app.use('/', authRoute);
+app.use('/', card);
 
 //Middleware
 app.use(require('./middlewares/auth.middleware.js'));
 
 //Rotas Particulares
-
 
 //Configuração de erro
 require('./error-handling')(app)
