d0fe4a1d0f6548efab0394d4105826cb11ff6f64
EuripedesNaves <euripedes.naves@outlook.com>
2022-12-15 21:23:38 -0300

    Ajusted gitignore

--- db/index.js
+++ db/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const MONGO_URI = "mongodb://localhost:27017/spellSeeker"
+const DB_URI = process.env.MONGO_URI
 
 const connect = async () => {
     try {
-        const x = await mongoose.connect(MONGO_URI)
+        const x = await mongoose.connect(DB_URI)
         console.log(`Connect to Mongo! DataBase: ${x.connections[0].name}`)
     }catch (error) {
         console.log(error)
